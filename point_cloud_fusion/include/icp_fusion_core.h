10218a065b9e027ea98107563227c16d2a91d48f
Jiaxing <39493524+ifaceyou123@users.noreply.github.com>
2019-01-23 13:44:27 +0100

    Add files via upload

--- point_cloud_fusion/include/icp_fusion_core.h
+++ point_cloud_fusion/include/icp_fusion_core.h
@@ -13,6 +13,7 @@
 #include <pcl/conversions.h>
 #include <pcl_ros/transforms.h>
 #include <pcl/registration/icp.h>
+#include <pcl/registration/transforms.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl_conversions/pcl_conversions.h>
 
@@ -28,13 +29,15 @@ class PointCloudFusion
 
 		ros::Publisher pub_filtered_points_;
 		
+		Eigen::Matrix4f GlobalTransform;
+
 		int counter;
 
 		void point_cb(const sensor_msgs::PointCloud2ConstPtr& in_cloud);
 
 		void publish_pointcloud(const ros::Publisher &in_publihser, const pcl::PointCloud<pcl::PointXYZRGB>::Ptr in_cloud_to_publish_ptr, const std_msgs::Header &in_header);
 		
-		void point_cloud_fusion(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr in, const pcl::PointCloud<pcl::PointXYZRGB>::Ptr out);
+		void point_cloud_fusion(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr in, const pcl::PointCloud<pcl::PointXYZRGB>::Ptr out, pcl::PointCloud<pcl::PointXYZRGB>::Ptr temp_out, Eigen::Matrix4f &final_transform);
 
  	public:
 		PointCloudFusion(ros::NodeHandle &nh);
--- point_cloud_fusion/src/icp_fusion_core.cpp
+++ point_cloud_fusion/src/icp_fusion_core.cpp
@@ -1,6 +1,6 @@
 #include "icp_fusion_core.h"
 
-PointCloudFusion::PointCloudFusion(ros::NodeHandle &nh):icp_result(new pcl::PointCloud<pcl::PointXYZRGB>),counter(0){
+PointCloudFusion::PointCloudFusion(ros::NodeHandle &nh):icp_result(new pcl::PointCloud<pcl::PointXYZRGB>),counter(0),GlobalTransform(Eigen::Matrix4f::Identity()){
 	
 //subscribe sensor_msgs::pointcloud2
 	sub_point_cloud_ = nh.subscribe("/iris_1/camera/depth/points", 10, &PointCloudFusion::point_cb, this);
@@ -26,7 +26,7 @@ void PointCloudFusion::publish_pointcloud(const ros::Publisher &in_publisher, co
 	in_publisher.publish(cloud_msg);
 }
 
-void PointCloudFusion::point_cloud_fusion(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr in, const pcl::PointCloud<pcl::PointXYZRGB>::Ptr out)
+void PointCloudFusion::point_cloud_fusion(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr in, const pcl::PointCloud<pcl::PointXYZRGB>::Ptr out, pcl::PointCloud<pcl::PointXYZRGB>::Ptr temp_out, Eigen::Matrix4f &final_transform)
 {
 //use ICP algorithm to fusion the point cloud 
 	pcl::IterativeClosestPoint<pcl::PointXYZRGB, pcl::PointXYZRGB> icp;
@@ -37,15 +37,30 @@ void PointCloudFusion::point_cloud_fusion(const pcl::PointCloud<pcl::PointXYZRGB
 //Difference between two iterations before and after
 	//icp.setEuclideanFitnessEpsilon (0.5);
 //set the maximum number of iterations
-	icp.setMaximumIterations (20); 
-
-
+	icp.setMaximumIterations (50); 
+// Run the same optimization in a loop 
+	Eigen::Matrix4f Ti = Eigen::Matrix4f::Identity (), prev, targetToSource;
 	icp.setInputSource (in);   // set source point cloud
 	icp.setInputTarget (out); //set target point cloud
 
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr result(new pcl::PointCloud<pcl::PointXYZRGB>);
 	icp.align (*result);
-	icp_result = result;
+
+//accumulate transformation between each Iteration
+	Ti = icp.getFinalTransformation () * Ti;
+
+// Get the transformation from target to source
+	targetToSource = Ti.inverse();
+
+// Transform target back in source frame
+	pcl::transformPointCloud (*out, *temp_out, targetToSource);
+
+	*temp_out += *in;
+
+	final_transform = targetToSource;
+
+	//icp_result = result;
+	std::cout <<  " score: " << icp.getFitnessScore() << std::endl;
 }
 
 // call back the point cloud and filter it, then use ICP to fusion point cloud to generate map 
@@ -54,6 +69,11 @@ void PointCloudFusion::point_cb(const sensor_msgs::PointCloud2ConstPtr & input_c
 
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr filtered_point_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr temp(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr final_result(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+	Eigen::Matrix4f pairTransform;
 // transform the pointcloud from ros type to pcl type
 	pcl::fromROSMsg(*input_cloud, *current_point_cloud);
 
@@ -61,24 +81,28 @@ void PointCloudFusion::point_cb(const sensor_msgs::PointCloud2ConstPtr & input_c
 //use voxel to filter the pointcloud, which from the sensor_msgs::pointcloud2
 	pcl::VoxelGrid<pcl::PointXYZRGB> vg;
 	vg.setInputCloud(current_point_cloud);
-	vg.setLeafSize(0.05f, 0.05f, 0.05f); 
+	vg.setLeafSize(0.07f, 0.07f, 0.07f); 
 	vg.filter(*filtered_point_cloud);
 	
 	counter = counter + 1 ;
 
 	if(counter == 1)
 	{
-		cout<<"has been "<<endl;
-		icp_result = filtered_point_cloud;
+		icp_result = filtered_point_cloud; //save the first frame as the source frame
 		cout<<"has been processed"<<endl;
 	}
 	else
 	{
-		point_cloud_fusion(filtered_point_cloud, icp_result);
+		point_cloud_fusion(icp_result, filtered_point_cloud, temp, pairTransform);
+//Convert the current point cloud temp after the registration to the global coordinate system and return result
+		pcl::transformPointCloud(*temp, *final_result, GlobalTransform);
+//Update the global transformation
+		GlobalTransform = GlobalTransform * pairTransform; 
+		icp_result = filtered_point_cloud;  //save the last target as the next source
 	}
-	if(counter % 10 == 0)
+	if(counter % 4 == 0)
 	{
-		publish_pointcloud(pub_filtered_points_, icp_result, input_cloud->header);
+	publish_pointcloud(pub_filtered_points_, final_result, input_cloud->header);
 	}
 	
 }
